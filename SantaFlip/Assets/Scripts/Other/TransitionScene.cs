372d00fd94611ef85466c1535b2a2640c15eeb93
hbkaraman <hakanburakkaraman@gmail.com>
2019-07-10 11:11:18 +0300

    Game Analytics Added

---
+++ SantaFlip/Assets/Scripts/Other/TransitionScene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameAnalyticsSDK;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScene : MonoBehaviour {
+
+	public Animator anim;
+	public string sceneName;
+	private bool isKeyPress = false;
+
+	private void Awake()
+	{
+		GameAnalytics.Initialize();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		StartCoroutine(LoadScene());
+	}
+
+	IEnumerator LoadScene()
+	{
+		anim.SetBool("IsCliked" , true);
+		yield return new WaitForSeconds(2);
+		SceneManager.LoadScene(sceneName);
+
+	
+	}
+
+}
